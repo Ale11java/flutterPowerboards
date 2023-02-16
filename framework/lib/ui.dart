4ab427020dde11c474c332b0c73d79d0fa5d3eca
josef <josef.kohout@timu.com>
2023-02-16 15:07:54 -0800

    Changing project file structure

---
+++ framework/lib/ui.dart
@@ -0,0 +1,8 @@
+/// TIMU Flutter UI widgets elements
+
+library timu_dart;
+
+export 'ui/screen_subtitle.dart';
+export 'ui/screen_text.dart';
+export 'ui/screen_title.dart';
+export 'ui/toolbar.dart';
--- lib/ui.dart
+++
@@ -1,8 +0,0 @@
-/// TIMU Flutter UI widgets elements
-
-library timu_dart;
-
-export 'src/ui/screen_subtitle.dart';
-export 'src/ui/screen_text.dart';
-export 'src/ui/screen_title.dart';
-export 'src/ui/toolbar.dart';
