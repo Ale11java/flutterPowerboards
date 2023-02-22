83201faa5c250c990b97a173bb3999b7b54d8e3d
josef <josef.kohout@timu.com>
2023-02-21 16:59:02 -0800

    Adding Websocket impl

--- framework/lib/websocket/websocket.dart
+++ framework/lib/websocket/websocket.dart
@@ -35,6 +35,8 @@ class Client {
   }
 }
 
+final Client emptyClient = Client('_empty_', <String, dynamic>{}, <String, dynamic>{});
+
 class UniqueClient extends Client {
   UniqueClient(
     super.id,
@@ -46,63 +48,186 @@ class UniqueClient extends Client {
   final List<Client> connections;
 }
 
-enum State {
-  open,
-  disconnected,
+class PongStream extends Stream<int> implements StreamSink<int> {
+  final StreamController<int> _controller = StreamController<int>.broadcast();
+
+  @override
+  StreamSubscription<int> listen(void Function(int event)? onData,
+          {Function? onError, void Function()? onDone, bool? cancelOnError}) =>
+      _controller.stream.listen(
+        onData,
+        onError: onError,
+        onDone: onDone,
+        cancelOnError: cancelOnError,
+      );
+
+  @override
+  void add(int event) => _controller.sink.add(event);
+
+  @override
+  Future<dynamic> close() => _controller.sink.close();
+
+  @override
+  Future<dynamic> addStream(Stream<int> source) =>
+      _controller.addStream(source);
+
+  @override
+  void addError(Object error, [StackTrace? stackTrace]) =>
+      _controller.addError(error, stackTrace);
+
+  @override
+  Future<dynamic> get done => _controller.done;
 }
 
-class Websocket {
-  Websocket(
-    this.apiRoot,
-    this.url,
-    this.accessToken,
-    this.network, {
+class TimuWebsocketEvent {
+  TimuWebsocketEvent(this.sender, this.data);
+
+  final String sender;
+  final Map<String, dynamic> data;
+}
+
+class TimuWebsocket extends Stream<TimuWebsocketEvent> {
+  TimuWebsocket({
+    required this.apiRoot,
+    required this.url,
+    required this.accessToken,
+    required this.network,
     this.channel = 'default',
     Map<String, dynamic>? metadata,
   })  : metadata = metadata ?? <String, dynamic>{},
         clients = <Client>[],
-        uniqueClients = <Client>[];
+        uniqueClients = <Client>[],
+        _pongStream = PongStream(),
+        _messages = StreamController<TimuWebsocketEvent>();
 
   final String apiRoot;
   final String url;
   final String accessToken;
   final String network;
+
   final String channel;
   final Map<String, dynamic> metadata;
+
+  final PongStream _pongStream;
 
   List<Client> clients;
   List<Client> uniqueClients;
 
-  WebSocketChannel? websocket;
-  bool closed = false;
-  State state = State.disconnected;
+  WebSocketChannel? _websocket;
+  final StreamController<TimuWebsocketEvent> _messages;
+
   bool synced = false;
+  int pongCount = 0;
+
+  StreamSubscription<int>? _pingPongSub;
+
+  @override
+  StreamSubscription<TimuWebsocketEvent> listen(
+          void Function(TimuWebsocketEvent event)? onData,
+          {Function? onError,
+          void Function()? onDone,
+          bool? cancelOnError}) =>
+      _messages.stream.listen(
+        onData,
+        onError: onError,
+        onDone: onDone,
+        cancelOnError: cancelOnError,
+      );
 
   void connect() {
-    websocket?.sink.close();
+    _websocket?.sink.close();
 
-    websocket = WebSocketChannel.connect(Uri.parse(_formatUrl(
+    _websocket = WebSocketChannel.connect(Uri.parse(_formatUrl(
         apiRoot, url, accessToken, network, channel, jsonEncode(metadata))));
+
+    _websocket?.stream.listen((dynamic res) {
+      final Map<String, dynamic> msg =
+          jsonDecode(res.toString()) as Map<String, dynamic>;
+
+      print('jkk $res'); // ignore: avoid_print
+
+      if (msg.containsKey('ListClients') && msg['ListClients'] != null) {
+        final Map<String, dynamic> data =
+            msg['ListClients'] as Map<String, dynamic>;
+
+        clients.clear();
+        for (final dynamic client in data['Clients'] as List<dynamic>) {
+          final Map<String, dynamic> item = client as Map<String, dynamic>;
+
+          clients.add(Client(
+            item['id'].toString(),
+            item['metadata'] as Map<String, dynamic>,
+            item['profile'] as Map<String, dynamic>,
+          ));
+        }
+      } else if (msg.containsKey('Pong') && msg['Pong'] != null) {
+        _pongStream.add(pongCount++);
+      } else if (msg.containsKey('Published') && msg['Published'] != null) {
+        final Map<String, dynamic> published =
+            msg['Published'] as Map<String, dynamic>;
+
+        final Map<String, dynamic> data =
+            jsonDecode(published.toString()) as Map<String, dynamic>;
+
+        if (data.containsKey('Added')) {
+          clients.add(Client(
+            published['Sender'].toString(),
+            data['Metadata'] as Map<String, dynamic>,
+            data['Added'] as Map<String, dynamic>,
+          ));
+
+        } else if (data.containsKey('Removed')) {
+          final String id = published['Sender'].toString();
+          clients.removeWhere((Client c) => c.id == id);
+
+        } else if (data.containsKey('Typing')) {
+          final String id = published['Sender'].toString();
+          final Client client = clients.firstWhere(
+            (Client c) => c.id == id,
+            orElse: () => emptyClient);
+          client.sendTyping();
+
+        } else {
+          _messages.add(TimuWebsocketEvent(
+            published['Sender'].toString(),
+            data,
+          ));
+        }
+      }
+    });
+
+    _websocket?.sink.add('{"ListClients":{}}');
+
+    _startPingPong();
   }
 
-  void _reconnect() {}
+  void _startPingPong() {
+    _pingPongSub?.cancel();
 
-  void _startPingPong() {}
+    _pingPongSub = Stream<int>.periodic(const Duration(seconds: 15), (int i) => i)
+        .listen((int i) async {
+          _ping();
 
-  void _ping() {}
+          final bool reconnect = await Future.any<bool>(<Future<bool>>[
+            _pongStream.first.then((int i) => false),
+            Future<dynamic>.delayed(const Duration(seconds: 5))
+              .then((dynamic v) => true),
+          ]);
 
-  void sendTyping() {}
+          if (reconnect) {
+            connect();
+          }
+        });
+  }
+
+  void _ping() {
+    _websocket?.sink.add('{"Ping":{}}');
+  }
 
   void close() {
-    closed = true;
+    _websocket?.sink.close();
+    _websocket = null;
 
-    websocket?.sink.close();
-    websocket = null;
-
-    state = State.disconnected;
+    _pingPongSub?.cancel();
   }
-
-  void publish() {}
-
-  void _syncClients() {}
 }
