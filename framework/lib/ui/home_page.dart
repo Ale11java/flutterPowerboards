2b7107b4ab3aef81a58e16dfe6cc0a8c9ddcab05
Owen Garmire <owen.garmire@timu.com>
2023-02-22 18:13:59 -0800

    Update sample home page to demonstrate storage login.

--- framework/lib/ui/home_page.dart
+++ framework/lib/ui/home_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'primary_button.dart';
+import '../model/account.dart';
+import 'auth_model.dart';
 import 'screen_subtitle.dart';
-import 'screen_text.dart';
 import 'screen_title.dart';
 import 'storage_logout_button.dart';
-import 'toolbar.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -26,99 +25,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final AuthModel props = AuthModel.of(context);
+    final Account activeAccount = props.activeAccount!;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: ScreenTitle(text: widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.all(30),
-              child: StorageLogoutButton(),
-            ),
-            // const StorageLogoutButton(),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const ScreenSubtitle(text: 'Design meeting'),
-            const ScreenText(
-                text: 'You are joining a TIMU meeting with a Powerboard'),
-            Toolbar(direction: ToolbarDirection.horizontal, children: const <
-                Widget>[
-              ScreenText(text: 'Tool 1'),
-              ScreenText(text: 'Tool 2'),
-              ToolbarSeparator(),
-              ScreenText(text: 'Tool 3'),
-              ToolbarButton(
-                  child: Text('libraries',
-                      style: TextStyle(color: Color(0xffffffff)))),
-              ToolbarButton(
-                  child:
-                      Text('hi', style: TextStyle(color: Color(0xffffffff)))),
-              ToggleToolbarButton(
-                  on: true,
-                  child:
-                      Text('hi', style: TextStyle(color: Color(0xffffffff)))),
-              EmphasizedToolbarButton(
-                  child:
-                      Text('hi', style: TextStyle(color: Color(0xffffffff)))),
-              EmphasizedToolbarButton(
-                  child:
-                      Text('hi', style: TextStyle(color: Color(0xffffffff)))),
-              ScreenText(text: 'Tool 4'),
-            ]),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: ScreenTitle(text: widget.title),
         ),
-      ),
-      floatingActionButton: RawMaterialButton(
-        onPressed: _incrementCounter,
-        child: const Text('T'),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Invoke "debug painting" (press "p" in the console, choose the
+                // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                // to see the wireframe for each widget.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              ScreenSubtitle(text: 'Welcome ${activeAccount.email}'),
+              const Padding(
+                padding: EdgeInsets.all(30),
+                child: StorageLogoutButton(),
+              ),
+            ])));
   }
 }
